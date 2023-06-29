619cc67091bc591ab85be7dffbb202aa73a2d7cb
Sunjang1009 <sunjang1009@gmail.com>
2023-06-28 22:35:56 -0500

    updated settings.py

--- spotify_project/settings.py
+++ spotify_project/settings.py
@@ -49,7 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main_app.apps.MainAppConfig',
-    'corsheaders',
+    # 'corsheaders',
 ]
 
 MIDDLEWARE = [
